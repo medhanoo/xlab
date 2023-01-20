a2210fdb806bd34fa2bb09aca1c882ce1f80ca43
DJ <git.medhanu@gmail.com>
2023-01-20 17:49:07 +0530

    bump to k 1.26.0

--- k8s-install.sh
+++ k8s-install.sh
@@ -4,25 +4,27 @@
 
 docker_install() {
 
-sudo apt-get update
+apt-get update
 
 
-sudo apt-get -y install \
+apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 
-sudo add-apt-repository \
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+#sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get install -y containerd.io
+containerd config default > /etc/containerd/config.toml
 
 cat > /etc/docker/daemon.json <<EOF
 	{
@@ -45,7 +47,7 @@ systemctl restart docker
 
 k8s_install() {
 
-apt-get install linux-modules-5.4.0-107-generic
+apt-get install -y linux-modules-5.15.0-58-generic
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
@@ -53,13 +55,12 @@ deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sudo apt-get update
 
-apt-get install -y  kubeadm=1.23.0-00 kubelet=1.23.0-00 kubectl=1.23.0-00
+apt-get install -y  kubeadm=1.26.0-00 kubelet=1.26.0-00 kubectl=1.26.0-00
 
 
 sudo apt-mark hold kubelet kubeadm kubectl
 
 }
-
 
 
 docker_install
